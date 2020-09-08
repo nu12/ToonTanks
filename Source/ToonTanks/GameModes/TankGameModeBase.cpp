c03843a030e2a1354e4d896f8593b32231601f33
Alysson A. Costa <alysson.avila.costa@gamil.com>
2020-09-08 17:10:57 -0300

    Remove magic number

--- Source/ToonTanks/GameModes/TankGameModeBase.cpp
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -34,7 +34,7 @@ void ATankGameModeBase::HandleGameStart()
 		GetWorld()->GetTimerManager().SetTimer(
 			EnableControllerTimerHandle,
 			EnableControllerDelegate,
-			StartDelay + 1,
+			StartDelay,
 			false
 		);
 	}
